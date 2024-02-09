c421c13944322aa4be240f68d86d532430638004
PhiphiLeBoss12 <100560205+PhiphiLeBoss12@users.noreply.github.com>
2024-02-09 19:33:26 +0100

    added a testable main

--- src/main/java/com/teamf5/gauntlet/Main.java
+++ src/main/java/com/teamf5/gauntlet/Main.java
@@ -1,7 +1,26 @@
 package com.teamf5.gauntlet;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public void start(Stage stage) throws Exception {
+        final Pane myPane = new Pane();
+
+        // Scene dimensions
+        double width = 300.0, height = 200.0;
+
+        // Scene = container for all content
+        Scene scene = new Scene(myPane, width, height);
+
+        stage.setScene(scene);
+        stage.setTitle("Basic Scene");
+        stage.show ();
     }
+
+//    public static void main(String[] args) {
+//        System.out.println("Hello world!");
+//    }
 }
