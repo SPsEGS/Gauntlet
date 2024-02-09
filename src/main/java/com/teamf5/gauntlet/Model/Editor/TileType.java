f59da8aec7cb11801842fd384d2666f1d63c9cb2
Toto <totooodev@proton.me>
2024-02-09 12:20:47 +0100

    Added TileType

---
+++ src/main/java/com/teamf5/gauntlet/Model/Editor/TileType.java
@@ -0,0 +1,24 @@
+package com.teamf5.gauntlet.Model.Editor;
+
+public enum TileType {
+    // Potions
+    GREEN_POTION_GOOD,
+    GREEN_POTION_BAD,
+    PURPLE_POTION,
+    BLUE_POTION,
+    RED_POTION,
+    WHITE_POTION,
+
+    // Items
+    KEY,
+    CHICKEN,
+    BOMB,
+
+    // Other tiles
+    GROUND,
+    WALL,
+    TREASURE_CHEST,
+    DOOR,
+    BONES_SPAWNER,
+    BOX_SPAWNER
+}
