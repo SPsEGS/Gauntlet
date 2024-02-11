4fe5177a23a3858346c47993054dc3202cf7783e
Baptiste Denis <baptiste.denis@etu.univ-poitiers.fr>
2024-02-11 01:17:33 +0100

    Add documentation in Editor.java

--- src/main/java/com/teamf5/gauntlet/View/Editor.java
+++ src/main/java/com/teamf5/gauntlet/View/Editor.java
@@ -5,8 +5,10 @@ import javafx.scene.layout.Pane;
 
 import java.io.IOException;
 
+/**
+ * The main window of the Editor, allowing the user to build a map with a visual tool, using a set of predefined tiles.
+ */
 public class Editor extends Pane {
-
     public Editor() {
         String fxmlFileName = "/View/EditorView.fxml";
 
