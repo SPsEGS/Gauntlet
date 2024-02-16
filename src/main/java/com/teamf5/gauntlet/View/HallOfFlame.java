6d5d762fb84d230873c83338036d7f96efe9c06f
RainDeRiv <marie.bruyant@yahoo.fr>
2024-02-16 11:26:39 +0100

    début de hall of flame

--- src/main/java/com/teamf5/gauntlet/View/HallOfFlame.java
+++ src/main/java/com/teamf5/gauntlet/View/HallOfFlame.java
@@ -1,4 +1,20 @@
 package com.teamf5.gauntlet.View;
 
-public class HallOfFlame {
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class HallOfFlame extends Pane {
+    public HallOfFlame() {
+        String fxmlFileName = "/View/HallOfFlameView.fxml";
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlFileName));
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException("HallOfFlame: Failed to load " + fxmlFileName);
+        }
+    }
 }
