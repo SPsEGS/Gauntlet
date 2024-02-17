03928b826de6c837537e6bcfd2fc04e94bc40203
Baptiste Denis <baptiste.denis@etu.univ-poitiers.fr>
2024-02-17 15:59:48 +0100

    Add proper textures to tile selection and documentation to EditorController

--- src/main/java/com/teamf5/gauntlet/Controller/EditorController.java
+++ src/main/java/com/teamf5/gauntlet/Controller/EditorController.java
@@ -1,6 +1,7 @@
 package com.teamf5.gauntlet.Controller;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -11,33 +12,45 @@ import javafx.scene.layout.GridPane;
 import com.teamf5.gauntlet.Model.Editor.TileType;
 import javafx.scene.layout.HBox;
 
-import java.io.IOException;
 import java.util.*;
 
 /**
  * The controller for the main view of the Editor.
  */
 public class EditorController {
+    /** The grid on which the game map is shown */
     @FXML
-    public GridPane grid;
+    private GridPane grid;
+
+    /** The TabPane containing the groups of buttons to select a tile */
     @FXML
     private TabPane tileSelect;
 
+    /** The different values of tiles that can be selected, organized in groups to be shown in different tabs */
     private final Map<String, List<TileType>> tileGroups = new HashMap<>();
 
-    /** Constructor (empty on purpose) */
+    /** Constructor (initializes all tile groups). */
     public EditorController() {
         initTileGroups();
     }
 
-    public void initialize() throws IOException {
+    /**
+     * Creates and adds all dynamically created UI elements to the editor (tile selection, map grid)
+     */
+    public void initialize() {
+        // Create a tab with an HBox for each tile group
         for(String group : tileGroups.keySet()) {
             Tab t = new Tab();
             t.setText(group);
-
             HBox hBox = new HBox();
+
+            // For each tile in the group, create a button with the correct texture
             for(TileType tile : tileGroups.get(group)) {
-                Button button = new Button("", new ImageView(getImage()));
+                ImageView imageView = new ImageView(getImage(tile));
+                // Crop the image to only show the first 80x80 pixels (some textures have several frames, only show the first)
+                imageView.setViewport(new Rectangle2D(0, 0, 79, 79));
+
+                Button button = new Button("", imageView);
                 hBox.getChildren().add(button);
             }
 
@@ -46,10 +59,73 @@ public class EditorController {
         }
     }
 
-    private Image getImage() {
-        return new Image(getClass().getResource("/Textures/Tiles/Potions/potion_magic.png").toExternalForm());
+    /**
+     * Gets the image corresponding to the given tile
+     * @param tile The tile to find the texture of
+     * @return An image of the chosen tile's texture
+     */
+    private Image getImage(TileType tile) {
+        String address;
+
+        switch(tile) {
+            case POTION_LIFE:
+                address = "/Textures/Tiles/Potions/potion_life.png";
+                break;
+            case POTION_POISON:
+                address = "/Textures/Tiles/Potions/potion_poison.png";
+                break;
+            case POTION_SPEED:
+                address = "/Textures/Tiles/Potions/potion_speed.png";
+                break;
+            case POTION_MAGIC:
+                address = "/Textures/Tiles/Potions/potion_magic.png";
+                break;
+            case POTION_PHYSICAL:
+                address = "/Textures/Tiles/Potions/potion_physical.png";
+                break;
+            case POTION_DEFENSE:
+                address = "/Textures/Tiles/Potions/potion_defense.png";
+                break;
+            case KEY:
+                address = "/Textures/Items/key.png";
+                break;
+            case CHICKEN:
+                address = "/Textures/Items/food.png";
+                break;
+            case BOMB:
+                address = "/Textures/Items/smart_bomb.png";
+                break;
+            case TREASURE_CHEST:
+                address = "/Textures/Items/treasure.png";
+                break;
+            case GROUND:
+                address = "/Textures/Tiles/Environment/floor.png";
+                break;
+            case WALL:
+                address = "/Textures/Tiles/Environment/wall.png";
+                break;
+            case DOOR:
+                address = "/Textures/Tiles/Environment/door.png";
+                break;
+            case BONES_SPAWNER:
+                address = "/Textures/Tiles/Spawners/spawner_ghost.png";
+                break;
+            case BOX_SPAWNER:
+                address = "/Textures/Tiles/Spawners/spawner_grunt.png";
+                break;
+            case EXIT:
+                address = "/Textures/Tiles/Environment/exit.png";
+                break;
+            default:
+                address = "bomb_animation"; // TODO: This is a placeholder, add an error texture?
+        }
+
+        return new Image(getClass().getResource(address).toExternalForm());
     }
 
+    /**
+     * Sort all tile types into groups and store them in the tileGroups HashMap.
+     */
     private void initTileGroups() {
         this.tileGroups.put("Potions", new ArrayList<>());
         this.tileGroups.get("Potions").add(TileType.POTION_LIFE);
